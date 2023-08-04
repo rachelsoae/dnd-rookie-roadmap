d223cdad822f53d0a762a58a5b303e374951ad6e
Rachel Prather <rachelprather1@gmail.com>
2023-08-03 21:08:59 -0700

    Fix bug where fetched data was being set in state twice

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ import Skill from './Skill';
 import RaceClass from './RaceClass';
 import Race from './Race';
 import Class from './Class';
-import Loading from './Loading';
 import { getData } from '../apiCalls';
 import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
@@ -24,7 +23,7 @@ function App() {
   const [classes, setClasses] = useState([]);
 
   useEffect(() => {
-    let rulesPromises = [];
+    let updateRules = true;
     const rule01 = getData('rule-sections/what-is-a-spell');
     const rule02 = getData('rule-sections/casting-a-spell');
     const rule03 = getData('rule-sections/time');
@@ -40,14 +39,17 @@ function App() {
     const rule13 = getData('rule-sections/saving-throws');
 
     Promise.all([rule01, rule02, rule03, rule04, rule05, rule06, rule07, rule08, rule09, rule10, rule11, rule12, rule13])
-    .then(responses => responses.forEach(response => rulesPromises.push(response)))
+    .then(responses => {
+      if (updateRules) {
+        responses.forEach(response => setRules(prevState => [...prevState, response]))
+      }
+    })
     .catch(error => alert(error))
-
-    return () => setRules([...rulesPromises])
+    return () => updateRules = false;
   }, [])
 
   useEffect(() => {
-    let abilitiesPromises = [];
+    let updateAbilities = true;
     const ability01 = getData('ability-scores/cha');
     const ability02 = getData('ability-scores/con');
     const ability03 = getData('ability-scores/dex');
@@ -56,15 +58,18 @@ function App() {
     const ability06 = getData('ability-scores/wis');
 
     Promise.all([ability01, ability02, ability03, ability04, ability05, ability06])
-    .then(responses => responses.forEach(response => abilitiesPromises.push(response)))
+    .then(responses => {
+      if (updateAbilities) {
+        responses.forEach(response => setAbilities((prevState => [...prevState, response])))
+      }
+   })
     .catch(error => alert(error))
 
-    return () => setAbilities([...abilitiesPromises])
-
+    return () => updateAbilities = false;
   }, [])
 
   useEffect(() => {
-    let skillsPromises = [];
+    let updateSkills = true;
     const skill01 = getData('skills/acrobatics');
     const skill02 = getData('skills/animal-handling');
     const skill03 = getData('skills/arcana');
@@ -85,15 +90,18 @@ function App() {
     const skill18 = getData('skills/survival');
 
     Promise.all([skill01, skill02, skill03, skill04, skill05, skill06, skill07, skill08, skill09, skill10, skill11, skill12, skill13, skill14, skill15, skill16, skill17, skill18])
-    .then(responses => responses.forEach(response => skillsPromises.push(response)))
+    .then(responses => {
+      if (updateSkills) {
+        responses.forEach(response => setSkills(prevState => [...prevState, response]))
+      }
+    })
     .catch(error => alert(error))
 
-    return () => setSkills([...skillsPromises])
-
+    return () => updateSkills = false;
   }, [])
 
   useEffect(() => {
-    let racesPromises = [];
+    let updateRaces = true;
     const race01 = getData('races/dragonborn');
     const race02 = getData('races/dwarf');
     const race03 = getData('races/elf');
@@ -105,15 +113,18 @@ function App() {
     const race09 = getData('races/tiefling');
 
     Promise.all([race01, race02, race03, race04, race05, race06, race07, race08, race09])
-    .then(responses => responses.forEach(response => racesPromises.push(response)))
+    .then(responses => {
+      if (updateRaces) {
+        responses.forEach(response => setRaces(prevState => [...prevState, response]))
+      }
+    })      
     .catch(error => alert(error))
 
-    return () => setRaces([...racesPromises])
-
+    return () => updateRaces = false;
   }, [])
 
   useEffect(() => {
-    let classesPromises = [];
+    let updateClasses;
     const charClass01 = getData('classes/barbarian');
     const charClass02 = getData('classes/bard');
     const charClass03 = getData('classes/cleric');
@@ -128,22 +139,18 @@ function App() {
     const charClass12 = getData('classes/wizard');
 
     Promise.all([charClass01, charClass02, charClass03, charClass04, charClass05, charClass06, charClass07, charClass08, charClass09, charClass10, charClass11, charClass12])
-    .then(responses => responses.forEach(response => classesPromises.push(response)))
+    .then(responses => {
+      if (updateClasses) {
+        responses.forEach(response => setClasses(prevState => [...prevState, response]))
+      }
+    })
     .catch(error => alert(error))
 
-    return () => setClasses([...classesPromises])
-
+    return () => updateClasses = false;
   }, [])
 
-  const [rule, setRule] = useState({});
-
-  const updateRule = (event) => {
-    console.log(event.target.id)
-    console.log(rules)
-    const currentRule = rules.find(rule => rule.index === event.target.id)
-    setRule(currentRule)
-  }
-
+  const [rule, setRule] = useState('');
+  
 
   return (
     <div className='app'>
@@ -155,14 +162,14 @@ function App() {
         <Nav />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/gameplay-basics' element={rules.length ? <Gameplay updateRule={updateRule} /> : <Loading />} />
+          <Route path='/gameplay-basics' element={<Gameplay rules={rules} setRule={setRule} />} />
           <Route path='/gameplay-basics/:id' element={<Rule rule={rule} />} />
-          <Route path='/abilities-and-skills' element={(abilities.length && skills.length) ? <AbilitySkill /> : <Loading />} />
-          <Route path='/abilities/:id' element={abilities.length ? <Ability abilities={abilities} /> : <Loading />} />
-          <Route path='/skills/:id' element={skills.length ? <Skill skills={skills} /> : <Loading />} />
-          <Route path='/races-and-classes' element={(races.length && classes.length) ? <RaceClass /> : <Loading />} />
-          <Route path='/races/:id' element={races.length ? <Race /> : <Loading />} />
-          <Route path='/classes/:id' element={classes.length ? <Class /> : <Loading />} />
+          <Route path='/abilities-and-skills' element={<AbilitySkill />} />
+          <Route path='/abilities/:id' element={<Ability abilities={abilities} />} />
+          <Route path='/skills/:id' element={<Skill skills={skills} />} />
+          <Route path='/races-and-classes' element={<RaceClass />} />
+          <Route path='/races/:id' element={<Race />} />
+          <Route path='/classes/:id' element={<Class />} />
         </Routes>
       </section>
     </div>
