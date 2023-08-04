38b49ab5d9eb579445afc33f9006a06f79b9d5a8
Rachel Prather <rachelprather1@gmail.com>
2023-08-04 16:55:17 -0700

    Add PropTypes

--- src/components/Race.js
+++ src/components/Race.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Race = ({abilities, race}) => {
   const relatedAbilities = race.ability_bonuses.map(currentAbility => {
     let fullAbility = abilities.find(ability => ability.index === currentAbility.ability_score.index)
@@ -16,3 +18,26 @@ const Race = ({abilities, race}) => {
 }
 
 export default Race;
+
+Race.propTypes = {
+  abilities: PropTypes.arrayOf(PropTypes.object),
+  race: PropTypes.shape({
+    "index": PropTypes.string,
+    "name": PropTypes.string,
+    "speed": PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    "ability_bonuses": PropTypes.arrayOf(PropTypes.object),
+    "ability_bonus_options": PropTypes.object,
+    "alignment":PropTypes.string,
+    "age": PropTypes.string,
+    "size": PropTypes.string,
+    "size_description": PropTypes.string,
+    "starting_proficiencies": PropTypes.array,
+    "starting_proficiency_options": PropTypes.object,
+    "languages": PropTypes.arrayOf(PropTypes.object),
+    "language_options": PropTypes.object,
+    "language_desc": PropTypes.string,
+    "traits": PropTypes.arrayOf(PropTypes.object),
+    "subraces": PropTypes.array,
+    "url": PropTypes.string
+})
+}
