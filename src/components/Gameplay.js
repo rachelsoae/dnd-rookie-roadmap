05f5a701b583643d734fe6daef169db520a2b30c
Rachel Prather <rachelprather1@gmail.com>
2023-08-04 18:11:53 -0700

    Dynamically render links

--- src/components/Gameplay.js
+++ src/components/Gameplay.js
@@ -2,6 +2,20 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Gameplay = ({ updateRule}) => { 
+  const rulesNames = {
+    spellcasting: ['What is a spell', 'Casting a spell'],
+    adventuring: ['Time', 'Resting'],
+    combat: ['The Order of Combat', 'Movement & Position', 'Actions in Combat', 'Making an Attack', 'Damage and Healing'], 
+    'using ability scores': ['Ability Checks', 'Using Each Ability', 'Proficiency Bonus', 'Saving Throws']
+  }
+  
+  const getRulesLinks = (category) => {
+    return rulesNames[category].map(name => {
+      const index = name.toLowerCase().split(' ').join('-');
+      return <NavLink key={`${index}`} to={`/gameplay-basics/${index}`} id={`${index}`} className='main__link' onClick={(e) => updateRule(e)}>{`${name}`}</NavLink>
+    })
+  }
+
   return (
     <main>
       <h2 className='main__heading'>Gameplay Basics</h2>
@@ -10,34 +24,25 @@ const Gameplay = ({ updateRule}) => {
         <section className='main__rule-section'>
           <h3 className='main__subheading'>Spellcasting</h3>
           <div className='main__links'>
-            <NavLink to='/gameplay-basics/what-is-a-spell' id='what-is-a-spell' className='main__link' onClick={(e) => updateRule(e)}>What is a spell?</NavLink>
-            <NavLink to='/gameplay-basics/casting-a-spell' id='casting-a-spell' className='main__link' onClick={(e) => updateRule(e)}>Casting a spell</NavLink>
+            {getRulesLinks('spellcasting')}
           </div>
         </section>
         <section className='main__rule-section'>
           <h3 className='main__subheading'>Adventuring</h3>
           <div className='main__links'>
-            <NavLink to='/gameplay-basics/time' id='time' className='main__link' onClick={(e) => updateRule(e)}>Time</NavLink>
-            <NavLink to='/gameplay-basics/resting' id='resting' className='main__link' onClick={(e) => updateRule(e)}>Resting</NavLink>
+            {getRulesLinks('adventuring')}
           </div>
         </section>
         <section className='main__rule-section'>
           <h3 className='main__subheading'>Combat</h3>
           <div className='main__links'>
-            <NavLink to='/gameplay-basics/the-order-of-combat' id='the-order-of-combat' className='main__link' onClick={(e) => updateRule(e)}>The Order of Combat</NavLink>
-            <NavLink to='/gameplay-basics/movement-and-position' id='movement-and-position' className='main__link' onClick={(e) => updateRule(e)}>Movement & Position</NavLink>
-            <NavLink to='/gameplay-basics/actions-in-combat' id='actions-in-combat' className='main__link' onClick={(e) => updateRule(e)}>Actions in Combat</NavLink>
-            <NavLink to='/gameplay-basics/making-an-attack' id='making-an-attack' className='main__link' onClick={(e) => updateRule(e)}>Making an Attack</NavLink>
-            <NavLink to='/gameplay-basics/damage-and-healing' id='damage-and-healing' className='main__link' onClick={(e) => updateRule(e)}>Damage and Healing</NavLink>
+            {getRulesLinks('combat')}
           </div>
         </section>
         <section className='main__rule-section'>
           <h3 className='main__subheading'>Using Ability Scores</h3>
           <div className='main__links'>
-            <NavLink to='/gameplay-basics/ability-checks' id='ability-checks' className='main__link' onClick={(e) => updateRule(e)}>Ability Checks</NavLink>
-            <NavLink to='/gameplay-basics/using-each-ability' id='using-each-ability' className='main__link' onClick={(e) => updateRule(e)}>Using Each Ability</NavLink>
-            <NavLink to='/gameplay-basics/proficiency-bonus' id='proficiency-bonus' className='main__link' onClick={(e) => updateRule(e)}>Proficiency Bonus</NavLink>
-            <NavLink to='/gameplay-basics/saving-throws' id='saving-throws' className='main__link' onClick={(e) => updateRule(e)}>Saving Throws</NavLink>
+            {getRulesLinks('using ability scores')}
           </div>
         </section>
       </div>
