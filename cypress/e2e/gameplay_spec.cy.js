deb4bbbaa59ea160372d3ef0e7aa82c7a216661f
Rachel Prather <rachelprather1@gmail.com>
2023-09-06 14:58:09 -0700

    Stub individual fetches dynamically

--- cypress/e2e/gameplay_spec.cy.js
+++ cypress/e2e/gameplay_spec.cy.js
@@ -1,27 +1,26 @@
 describe('Gameplay Basics Page', () => {
   it('should display the page, click links, and render the correct pages', () => {
-    cy.getAllData()
     cy.visit('http://localhost:3000/') 
-    cy.wait('@lastFetch').then((interception) => {
-      cy.get('.nav__container').children().contains('Gameplay Basics').click()
-      cy.get('h2').contains('Gameplay Basics');
-      cy.get(':nth-child(1) > .main__subheading').contains('Spellcasting');
-      cy.get(':nth-child(2) > .main__subheading').contains('Adventuring');
-      cy.get(':nth-child(3) > .main__subheading').contains('Combat');
-      cy.get(':nth-child(4) > .main__subheading').contains('Ability Scores');
-      cy.checkLink('gameplay-basics', 'what-is-a-spell', 'what-is-a-spell', 'What Is a Spell?');
-      cy.checkLink('gameplay-basics', 'casting-a-spell', 'casting-a-spell', 'Casting a Spell');
-      cy.checkLink('gameplay-basics', 'time', 'time', 'Time');
-      cy.checkLink('gameplay-basics', 'resting', 'resting', 'Resting');
-      cy.checkLink('gameplay-basics', 'the-order-of-combat', 'the-order-of-combat', 'The Order of Combat');
-      cy.checkLink('gameplay-basics', 'movement-and-position',  'movement-and-position','Movement and Position');
-      cy.checkLink('gameplay-basics', 'actions-in-combat', 'actions-in-combat', 'Actions in Combat');
-      cy.checkLink('gameplay-basics', 'making-an-attack', 'making-an-attack', 'Making an Attack');
-      cy.checkLink('gameplay-basics', 'damage-and-healing', 'damage-and-healing', 'Damage and Healing');
-      cy.checkLink('gameplay-basics', 'ability-checks',  'ability-checks', 'Ability Checks');
-      cy.checkLink('gameplay-basics', 'using-each-ability',  'using-each-ability', 'Using Each Ability');
-      cy.checkLink('gameplay-basics', 'proficiency-bonus',  'proficiency-bonus', 'Proficiency Bonus');
-      cy.checkLink('gameplay-basics', 'saving-throws', 'saving-throws', 'Saving Throws');
-    })
+    cy.get('.nav__container').children().contains('Gameplay Basics').click()
+    cy.get('h2').contains('Gameplay Basics');
+    cy.get(':nth-child(1) > .main__subheading').contains('Spellcasting');
+    cy.get(':nth-child(2) > .main__subheading').contains('Adventuring');
+    cy.get(':nth-child(3) > .main__subheading').contains('Combat');
+    cy.get(':nth-child(4) > .main__subheading').contains('Ability Scores');
+
+    const NAMES = {
+      'gameplay-basics': {
+          spellcasting: ['What is a Spell', 'Casting a Spell'],
+          adventuring: ['Time', 'Resting'],
+          combat: ['The Order of Combat', 'Movement and Position', 'Actions in Combat', 'Making an Attack', 'Damage and Healing'], 
+          'using ability scores': ['Ability Checks', 'Using Each Ability', 'Proficiency Bonus', 'Saving Throws']
+        },
+      abilities: ['Charisma', 'Constitution', 'Dexterity', 'Intelligence', 'Strength', 'Wisdom'],
+      skills:['Acrobatics', 'Animal Handling', 'Arcana', 'Athletics', 'Deception', 'History', 'Insight', 'Intimidation', 'Investigation', 'Medicine', 'Nature', 'Perception', 'Performance', 'Persuasion', 'Religion', 'Sleight of Hand', 'Stealth', 'Survival'],
+      races: ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Tiefling'],
+      classes:['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+    }
+
+    Object.keys(NAMES['gameplay-basics']).forEach(key => cy.getLinks('gameplay-basics', key, 200))
   })
 })
